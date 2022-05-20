5d74895f556c522442ccdbc8426a3e5c57000cb4
jiwaraga06 <dermawanraga066@gmail.com>
2022-05-20 10:36:35 +0700

    Update perubahan

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      home: SocketChart(),
+      home: LineChartSample1(),
     );
   }
 }
